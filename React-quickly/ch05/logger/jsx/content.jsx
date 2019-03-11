c6482b6d7aebdc7edf915023974f2c8c84f5f049
Carlos3dg <carlosoo3dg@gmail.com>
2019-03-10 22:54:39 -0600

    Add Lifecycle events topic

---
+++ React-quickly/ch05/logger/jsx/content.jsx
@@ -0,0 +1,24 @@
+class Content extends React.Component {
+  constructor(props) {
+    super(props)
+    this.launchClock()
+    this.state = {
+      counter: 0,
+      currentTime: (new Date()).toLocaleString()
+    }
+  }
+  launchClock() {
+    setInterval(()=>{
+      this.setState({
+        counter: ++this.state.counter,
+        currentTime: (new Date()).toLocaleString()
+      })
+    }, 1000)
+  }
+  render() {
+    //Here we make the condition that says if the counter property of the state object is greater than 2 then we just render an empty div element if the condition is false we render the <Logger> component with the time attribute equal to the currentTime property of the state
+    if (this.state.counter > 2) return <div/>
+    //The time property is modify every time that the currentTime changes, so this property state changes every second in the LaunchClock method whose execute the setState to rerender the new element 
+    return <Logger time={this.state.currentTime}></Logger>
+  }
+}
---
+++ React-quickly/ch05/logger/jsx/logger.jsx
@@ -0,0 +1,50 @@
+class Logger extends React.Component {
+  constructor(props) {
+    super(props)
+    console.log('constructor') //Executed at the beggining, in the instance 
+  }
+  //Event executed before mounting the component to the DOM
+  componentWillMount() {
+    console.log('componentWillMount is triggered')
+  }
+  //Event executed after the component has been mounted to the DOM, here we have access to the element in the real DOM
+  componentDidMount(e) {
+    console.log('componentDidMount is triggered')
+    console.log('DOM node: ', ReactDOM.findDOMNode(this)) //This function print the component element itself, as it supposed to be in DOM
+  }
+  //Every time that a property is added or modify this function is executed before to add this kind of facts
+  componentWillReceiveProps(newProps) {
+    console.log('componentWillReceiveProps is triggered')
+    console.log('new props: ', newProps)
+  }
+  //This event is attached according with the update of the component, this means, a modification in its state or properties, and help us to decide if the updated shuld be executed or not
+  shouldComponentUpdate(newProps, newState) {
+    console.log('shouldComponentUpdate is triggered')
+    console.log('new props: ', newProps)
+    console.log('new state: ', newState)
+    return true //Here we return true to execute or aprove the update
+  }
+  //If the update is aproved we can execute an event before starting the update
+  componentWillUpdate(newProps, newState) {
+    console.log('componentWillUpdate is triggered')
+    console.log('new props: ', newProps)
+    console.log('new state: ', newState)
+  }
+  //And when the update is done we can execute this event
+  componentDidUpdate(oldProps, oldState) {
+    console.log('componentDidUpdate is triggered')
+    console.log('old props: ', oldProps)
+    console.log('old state: ', oldState)
+    //It is important to know that the last four events are executed when the component is updated (modifying the state or properties), but just the componentWillReceiveoProps is executed if a property is modify or added
+  }
+  //This event is executed before the component is removed, in this program this event is executed when the counter property in the state from the Content component is greater that 2
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+  }
+  render() {
+    console.log('rendering... Display')
+    return (
+      <div>{this.props.time}</div>
+    )
+  }
+}
