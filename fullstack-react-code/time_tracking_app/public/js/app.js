ffbfe6cc1fb28c565f6d160aaacb8ea83229adaa
Carlos3dg <carlosoo3dg@gmail.com>
2019-07-24 11:53:17 -0500

    Timer App - Step 2: Create EditableTimer and TimerForm components

--- fullstack-react-code/time_tracking_app/public/js/app.js
+++ fullstack-react-code/time_tracking_app/public/js/app.js
@@ -35,3 +35,59 @@ class EditableTimerList extends React.Component {
         );
     }
 }
+
+//Component to render a TimerForm or a Timer component as part of the EditableTimerList
+class EditableTimer extends React.Component {
+    render() {
+        //Here we determine if it is necessary to render the TimerForm or the Timer component
+        if (this.props.editFormOpen) {
+            return (
+                <TimerForm 
+                    title={this.props.title} 
+                    project={this.props.project} //We pass the title and project property values to take them as default values in the inputs sons from TimerForm
+                />
+            );
+        } else {
+            return(
+                <Timer 
+                    title={this.props.title}
+                    project={this.props.project}
+                    elapsed={this.props.elapsed}
+                    runningSince={this.props.runningSince}
+                />
+            );
+        }
+    }
+}
+
+//Component to render a Form whether to create a Timer or update it 
+class TimerForm extends React.Component {
+    render() {
+        const submitText = this.props.title ? 'Update' : 'Create';
+
+        return(
+            <div className="ui centered card">
+                <div className="content">
+                    <div className="ui form">
+                        <div className="field">
+                            <label>Title</label>
+                            <input type="text" defaultValue={this.props.title} />
+                        </div>
+                        <div className="field">
+                            <label>Project</label>
+                            <input type="text" defaultValue={this.props.project} />
+                        </div>
+                        <div className="ui two bottom attached buttons">
+                            <button className="ui basic blue button">
+                                {submitText}
+                            </button>
+                            <button className="ui basic red button">
+                                Cancel
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
