13b6d87e0142fe364ed20bfd6e85555b2d64f781
Carlos3dg <carlosoo3dg@gmail.com>
2020-03-26 08:17:44 -0600

    ModerReact: add axios to our pics project

--- ModernReact/pics/src/components/App.js
+++ ModernReact/pics/src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import axios from 'axios';
+//Axios is a package from npm to fetch data, this option is an alternative way of the fetch method. Although is a package an it has to install some files to our app, this option makes the development easier than fetch and allow us to write less code and have a better Development Experience.
 import SearchBar from './SearchBar';
 
-const App = ()=>{
-    return (
-        <div className="ui container" style={{marginTop: "10px"}}>
-            <SearchBar/>
-        </div>
-    );
+class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: "10px"}}>
+                <SearchBar onSubmit={this.onSearchSubmit}/>
+            </div>
+        );
+    }
 }
 
 export default App;
