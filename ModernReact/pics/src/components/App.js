de5fc47d20bc7dbed9c96a39749551a25b8cd096
Carlos3dg <carlosoo3dg@gmail.com>
2020-03-30 20:49:25 -0600

    Modern React: redered the image list with its respective key

--- ModernReact/pics/src/components/App.js
+++ ModernReact/pics/src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import unsplash from '../api/unsplash'; //Get the instance config of axios
 //Axios is a package from npm to fetch data, this option is an alternative way of the fetch method. Although is a package an it has to install some files to our app, this option makes the development easier than fetch and allow us to write less code and have a better Development Experience.
 import SearchBar from './SearchBar';
+import ImageList from './ImagesList';
 
 class App extends React.Component {
     state = {
@@ -21,7 +22,7 @@ class App extends React.Component {
         return (
             <div className="ui container" style={{marginTop: "10px"}}>
                 <SearchBar onSubmit={this.onSearchSubmit}/>
-                Found: {this.state.images.length} images
+                <ImageList images= {this.state.images} />
             </div>
         );
     }
