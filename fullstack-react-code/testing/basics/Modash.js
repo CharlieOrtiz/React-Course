7847b7ad3a40309e0d2ef8a869e69ce54feafa4a
Carlos3dg <carlosoo3dg@gmail.com>
2020-04-09 23:00:22 -0500

    FullSatck React: basic testing without any framework

--- fullstack-react-code/testing/basics/Modash.js
+++ fullstack-react-code/testing/basics/Modash.js
@@ -1,1 +1,30 @@
 // We write the Modash library in this file in the Unit Testing chapter
+//1
+function truncate(string, limit) {
+    if(string.length > limit) {
+        return string.slice(0, limit) + '...';
+    } else {
+        return string;
+    }
+}
+
+function capitalize(string) {
+    return (
+        string.charAt(0).toUpperCase() + string.slice(1).toLowerCase()
+    );
+}
+
+function camelCase(string) {
+    const words = string.split(/[\s|\-|_]+/);
+    return [
+        words[0].toLowerCase(), ...words.slice(1).map((word)=>capitalize(word)),
+    ].join('');
+}
+
+const Modash = {
+    truncate,
+    capitalize,
+    camelCase
+};
+
+export default Modash;
--- fullstack-react-code/testing/basics/Modash.test.js
+++ fullstack-react-code/testing/basics/Modash.test.js
@@ -1,2 +1,42 @@
 // We write the tests for the Modash library in
 // this file in the Unit Testing chapter
+import Modash from './Modash';
+
+function assertEqual(description, actual, expected) {
+    if(actual === expected) {
+        console.log(`[PASS] ${description}`);
+    } else {
+        console.log(`[FAIL] ${description}`);
+        console.log(`\tactual: '${actual}'`);
+        console.log(`\texpected: '${expected}'`);
+    }
+}
+
+let actual;
+let expected;
+let string;
+//TRUNCATE
+string = 'web development is just the beginning of this road';
+actual = Modash.truncate(string, 37);
+expected = 'web development is just the beginning...'
+
+assertEqual('`truncate()`: truncates a string', actual, expected);
+
+//CAPITALIZE
+actual = Modash.capitalize(string);
+expected = 'Web development is just the beginning of this road'
+
+assertEqual(`'capitalize()': capitalizes the string`, actual, expected);
+
+//camelCase
+string = 'html css js react redux grapqh node mogodb';
+actual = Modash.camelCase(string);
+expected = 'htmlCssJsReactReduxGrapqhNodeMogodb';
+
+assertEqual(`'camelCase()': string with spaces`, actual, expected);
+
+string = 'html-css-js-react-redux-grapqh-node-mogodb';
+actual = Modash.camelCase(string);
+expected = 'htmlCssJsReactReduxGrapqhNodeMogodb';
+
+assertEqual(`'camelCase()': string with dashes`, actual, expected);
