ba94f24fdd612ec95b3d7a7750b6c0e026ffdfa4
Carlos3dg <carlosoo3dg@gmail.com>
2020-04-17 10:41:56 -0500

    Fullstack React: Enzyme basics

--- fullstack-react-code/testing/basics/Modash.test.js
+++ fullstack-react-code/testing/basics/Modash.test.js
@@ -8,7 +8,26 @@ describe('Modash', ()=>{
 
         it('truncates a string', ()=>{
             expect(Modash.truncate(string, 18)).toEqual('web development is...');
-        }) ;
+        });
+    });
+
+    describe('capitalize()', ()=> {
+        it('capitalizes first letter, lowercases the rest', ()=> {
+            const string = 'web development is just the beginning';
+            expect(Modash.capitalize(string)).toEqual('Web development is just the beginning');
+        });
+    });
+
+    describe('camelCase()', ()=> {
+        it('camelizes string with spaces', ()=> {
+            const string = 'web development is just the beginning';
+            expect(Modash.camelCase(string)).toEqual('webDevelopmentIsJustTheBeginning');
+        });
+
+        it('camelizes string with underscores', ()=> {
+            const string = 'web_development_is_just_the_beginning';
+            expect(Modash.camelCase(string)).toEqual('webDevelopmentIsJustTheBeginning');
+        });
     });
 });
 
--- fullstack-react-code/testing/react-basics/src/App.test.js
+++ fullstack-react-code/testing/react-basics/src/App.test.js
@@ -1,8 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+//1. Asser that we have a table header with Items as its content
+describe('App', () => {
+  let wrapper; //The variables is globally to be available in all specs.
+ //beforeEach is a special function that runs just before every it block, we use this kind of functions to declare some special setup before running any assertion 
+  beforeEach(() => {
+    wrapper = shallow(
+      <App/>
+    ); //In this case we need to have our shallow object to be render every time we need in our assertions. In this way we guarantee that every time that we use our wrapper in our assertions we are using a fresh set of context.
+  });
+
+  it('should have the `th` "Items"', () => {
+    expect(
+      wrapper.contains(<th>Items</th>)
+      ).toEqual(true);
+  });
+
+  it('should have a `button` element', () => {
+    expect(
+      wrapper.containsMatchingElement(<button>Add item</button>)
+    ).toBe(true);
+  });
+
+  it('should have an `input` element', () => {
+    expect(
+      wrapper.containsMatchingElement(<input/>)
+    ).toBe(true);
+  });
+
+  it('button should be disabled', () => {
+    const button = wrapper.find('button').first();
+
+    expect(
+      button.props().disabled //Here we get the value of the disabled property, in this case, disabled is equal to true
+    ).toBe(true);
+     
+  });
+}); 
