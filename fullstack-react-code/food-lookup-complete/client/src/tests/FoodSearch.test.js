90cef02c6ee63e3d8dcfecc7d423834e52f02b38
Carlos3dg <carlosoo3dg@gmail.com>
2020-04-28 09:10:30 -0500

    Fullstack React: Testing food lookup app - first steps

--- fullstack-react-code/food-lookup-complete/client/src/tests/FoodSearch.test.js
+++ fullstack-react-code/food-lookup-complete/client/src/tests/FoodSearch.test.js
@@ -6,13 +6,48 @@ import FoodSearch from '../FoodSearch';
 
 describe('FoodSearch', () => {
   // ... initial state specs
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(
+      <FoodSearch/>
+    );
+  });
+
+  it('should not display the remove icon', () => {
+    expect(
+      wrapper.find('.remove.icon').length
+    ).toBe(0);
+  });
+
+  it('should display zero rows', () => {
+    expect(
+      wrapper.find('tbody tr').length
+    ).toBe(0);
+  });
 
   describe('user populates search field', () => {
+    const value = 'brocc';
     beforeEach(() => {
       // ... simulate user typing "brocc" in input
+      const input = wrapper.find('input').first();
+      input.simulate('change', {
+        target: { value: value }
+      });
+
     });
 
     // ... specs
+    it('should update state property `searchValue`', () => {
+      expect(
+        wrapper.state().searchValue
+      ).toEqual(value);
+    });
+
+    it('should display the remove icon', () => {
+      expect(
+        wrapper.find('.remove.icon').length
+      ).toBe(1);
+    });
 
     describe('and API returns results', () => {
       beforeEach(() => {
