d33c1bd9fc88f8ec8983c5a7e83acc5bd9305742
Carlos3dg <carlosoo3dg@gmail.com>
2020-05-05 23:28:23 -0500

    Fullsatck React: React Router - Traversing the APIs using context and Router component

--- fullstack-react-code/routing/basics/src/App.js
+++ fullstack-react-code/routing/basics/src/App.js
@@ -1,70 +1,116 @@
 import React from 'react';
 //Import the createBrowserHistory mtehod to get the history object, in this case this history object has some special methods not included in the browser API provided by HTML5 and prevent us to any incompatibility in the different browsers 
 import { createBrowserHistory } from 'history'
-
-const history = createBrowserHistory() //Get the history object
+//Variable use to define our context and define our Router state
+const contextObject = {
+  history: createBrowserHistory(),
+  location: window.location
+};
+const RouterContext = React.createContext(contextObject); //The defaul value. In case that one of our components is not inside a provider then its consumer (if it's using) will take this value)
 
 //Component that determines what to render according with the location (URL) of our app.
 const Route = ({path, component: Component}) => {
-  const pathname = window.location.pathname; //The window object; here we're accesing to the pathname (the directions after the hostname)
-  if(pathname.match(path)) {
-    return (
-      <Component/>
-      );
-  } else {
-    return null;
-  }
+  return <RouterContext.Consumer>
+    { ({location: {pathname}}) => {
+      if(pathname.match(path)) {
+        return <Component/>
+      } else {
+        return null;
+      }
+    }}
+  </RouterContext.Consumer>
 }
 
 //Component that change the location of our App without any browser request to the server.
 const Link = ({to, children}) => {
-  return (
-    <a onClick={(e) => {
-      e.preventDefault(); //PreventDefault deletes any page freshing
-      history.push(to); //Change the actual history stack and also updates the URL
+  return <RouterContext.Consumer>
+    { ({history}) => {
+      return (
+        <a onClick={(e) => {
+          e.preventDefault(); //PreventDefault deletes any page freshing
+          history.push(to); //Change the actual history stack and also updates the URL
+        }}
+          href={to}
+        >
+          {children}
+        </a>
+      )
     }}
-      href={to}
-    >
-      {children}
-    </a>
-  );
+  </RouterContext.Consumer>
+}
+
+class Router extends React.Component {
+  //State equal to the defaul context value
+  state = {
+    apis: contextObject
+  }
+  //Use componentDidMount to declare the listen method from history, this will change our state and invokes a re-render
+  componentDidMount() {
+    //When the listen method will be invoked it's going to change our state value equal to the actual history stack and then makes a re-render to mount the provider with the updated value
+    this.state.apis.history.listen(() => {
+      this.setState({
+        apis: {
+          history: createBrowserHistory(),
+          location: window.location
+        }
+      });
+    });
+  }
+  //Because componentDidMount just is attached one time during the lifecycle we need to declare the listen method again after the first update made by the componentDidMount
+  componentDidUpdate() {
+    this.state.apis.history.listen(() => {
+      this.setState({
+        apis: {
+          history: createBrowserHistory(),
+          location: window.location
+        }
+      });
+    });
+  }
+ 
+  render() {
+    return (
+      //Use context provider with a value equal to state to pass it to the rest of the child components, in this case Route and Link
+      <RouterContext.Provider value={this.state.apis}>
+        {this.props.children}
+      </RouterContext.Provider>
+    );
+  }
 }
 
 class App extends React.Component {
-  componentDidMount() {
-    //Now that our app is not refreshing we need a way to attach a re-render, in order to do that we use history.listen() to invoke it any time the history stack is changed, this method accepts a function as an argument in this time we define the react method forceUpdate().
-    history.listen(() => this.forceUpdate());
-  }
-
   render() {
     return (
-      <div
-        className='ui text container'
-      >
-        <h2 className='ui dividing header'>
-          Which body of water?
-        </h2>
+      <Router>
+        
+        <div
+          className='ui text container'
+        >
+          <h2 className='ui dividing header'>
+            Which body of water?
+          </h2>
 
-        <ul>
-          <li>
-            <Link to='/atlantic'> 
-              <code>/atlantic</code>
-            </Link>
-          </li>
-          <li>
-            <Link to='/pacific'> 
-              <code>/pacific</code>
-            </Link>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <Link to='/atlantic'> 
+                <code>/atlantic</code>
+              </Link>
+            </li>
+            <li>
+              <Link to='/pacific'> 
+                <code>/pacific</code>
+              </Link>
+            </li>
+          </ul>
 
-        <hr />
+          <hr />
 
-        {/* We'll insert the Route components here */} 
-        <Route path='/atlantic' component={Atlantic} /> {/* When we pass a component as a property, we don't instantiate it we just pass the function variable */}
-        <Route path='/pacific' component={Pacific} />
+          {/* We'll insert the Route components here */} 
+          <Route path='/atlantic' component={Atlantic} /> {/* When we pass a component as a property, we don't instantiate it we just pass the function variable */}
+          <Route path='/pacific' component={Pacific} />
 
-      </div>
+        </div>
+      </Router>
     );
   }
 }
