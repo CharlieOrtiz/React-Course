86b698aecc567c12cef1a449ef25f404ab963ded
Carlos3dg <carlosoo3dg@gmail.com>
2020-05-28 23:37:46 -0500

    Fullstack React: Redux - Beginnig the chat simple app

--- fullstack-react-code/redux/chat_simple/src/App.js
+++ fullstack-react-code/redux/chat_simple/src/App.js
@@ -1,5 +1,9 @@
-function createStore(reducer) {
-  let state = 0;
+const initialState = {
+  message: []
+};
+
+function createStore(reducer, initialState) {
+  let state = initialState;
 
   const getState = () => (state);
 
@@ -12,3 +16,34 @@ function createStore(reducer) {
     dispatch,
   };
 }
+
+function reducer(state, action) {
+  if(action.type === 'ADD_MESSAGE') {
+    return {
+      message: state.message.concat(action.message)
+    };
+  } else {
+    return state;
+  }
+}
+
+const store = createStore(reducer, initialState);
+
+const addMessage1 = {
+  type: 'ADD_MESSAGE',
+  message: 'Hi, how does it look?'
+}
+
+store.dispatch(addMessage1);
+const stateV1 = store.getState();
+
+const addMessage2 = {
+  type: 'ADD_MESSAGE',
+  message: 'Looking good, what about you?'
+}
+
+store.dispatch(addMessage2);
+const stateV2 = store.getState();
+
+console.log(stateV1);
+console.log(stateV2);
