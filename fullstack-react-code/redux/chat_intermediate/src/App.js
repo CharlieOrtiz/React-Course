22540393ce08d4c3e18b59a445a4b8fa64dd7c83
Carlos3dg <carlosoo3dg@gmail.com>
2020-06-05 09:48:04 -0500

    Fullstack React: Intermediate Redux - Adding tabs to our state and react components

--- fullstack-react-code/redux/chat_intermediate/src/App.js
+++ fullstack-react-code/redux/chat_intermediate/src/App.js
@@ -21,7 +21,27 @@ function reducer(state, action) {
   }
 }
 
-const initialState = { messages: [] };
+const initialState = { 
+  activeThreadId: '1-fca2',
+  threads: [
+    {
+      id: '1-fca2',
+      title: 'Buzz Aldrin',
+      messages: [
+        {
+          text: 'Twelve minutes to ignition.',
+          timestamp: Date.now(),
+          id: uuid.v4(),
+        },
+      ]
+    },
+    {
+      id: '2-be91',
+      title: 'Michael Collins',
+      messages: []
+    }
+  ] 
+};
 
 const store = createStore(reducer, initialState);
 
@@ -31,12 +51,73 @@ class App extends React.Component {
   }
 
   render() {
-    const messages = store.getState().messages;
+    const state = store.getState();
+    const activeThreadId = state.activeThreadId;
+    const threads = state.threads;
+    const activeThread = threads.find((thread) => activeThreadId === thread.id);
+
+    const tabs = threads.map((t) => (
+      {
+        title: t.title,
+        active: t.id === activeThreadId,
+      }
+    ));
 
     return (
       <div className='ui segment'>
-        <MessageView messages={messages} />
-        <MessageInput />
+        <ThreadTabs tabs={tabs}/>
+        <Thread thread={activeThread} />
+      </div>
+    );
+  }
+}
+
+class ThreadTabs extends React.Component {
+  render() {
+    const tabs = this.props.tabs.map((tab, index) => (
+      <div
+        key={index}
+        className={tab.active ? 'active item' : 'item'}
+      >
+        {tab.title}
+      </div>
+    ));
+
+    return (
+      <div className='ui top attached tabular menu'>
+        {tabs}
+      </div>
+    );
+  }
+}
+
+class Thread extends React.Component {
+  handleClick = (id) => {
+    store.dispatch({
+      type: 'DELETE_MESSAGE',
+      id: id,
+    });
+  };
+
+  render() {
+    const messages = this.props.thread.messages.map((message, index) => (
+      <div
+        className='comment'
+        key={index}
+        onClick={() => this.handleClick(message.id)}
+      >
+        <div className='text'>
+          {message.text}
+          <span className="metadata">@{message.timestamp}</span>
+        </div>
+      </div>
+    ));
+    return (
+      <div className='ui center aligned basic segment'>
+        <div className='ui comments'>
+          {messages}
+        </div>
+        <MessageInput/>
       </div>
     );
   }
@@ -79,37 +160,6 @@ class MessageInput extends React.Component {
           Submit
         </button>
        </div>
-    );
-  }
-}
-
-class MessageView extends React.Component {
-  handleClick = (id) => {
-    store.dispatch({
-      type: 'DELETE_MESSAGE',
-      id: id,
-    });
-  };
-
-  render() {
-    const messages = this.props.messages.map((message, index) => (
-      <div
-        className='comment'
-        key={index}
-        onClick={() => this.handleClick(message.id)}
-      >
-        <div className='text'>
-          {message.text}
-          <span className="metadata">@{message.timestamp}</span>
-        </div>
-      </div>
-    ));
-    return (
-      <div className='ui center aligned basic segment'>
-        <div className='ui comments'>
-          {messages}
-        </div>
-      </div>
     );
   }
 }
