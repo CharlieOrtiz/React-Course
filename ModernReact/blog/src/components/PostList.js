90402a9595fed7b3b1a38286e1b38d5e772ccc28
Carlos3dg <carlosoo3dg@gmail.com>
2020-06-27 00:28:59 -0500

    Modern React: Connect() and actions creators - Using mapStateToProps to avoid writting logic inside our presentational components

--- ModernReact/blog/src/components/PostList.js
+++ ModernReact/blog/src/components/PostList.js
@@ -1,18 +1,42 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions/index';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
     componentDidMount() {
         this.props.fetchPosts()
     }
 
+    renderList() {
+         return this.props.posts.map((post) => (
+             <div className='item' key={post.id}>
+                 <i className='large middle aligned icon user'></i>
+                 <div className='content'>
+                    <div className='description'>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                    </div>
+                    <UserHeader userId={post.userId} />
+                 </div>
+             </div>
+         ))
+    }
+
     render() {
-        return <div>PostList</div>
+        return <div className='ui relaxed divided list'>
+            {this.renderList()}
+        </div>
     }
 };
 
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    };
+};
+
 export default connect(
-    null, 
+    mapStateToProps, 
     {fetchPosts} //mapDispatchToProps; remember this function return an object with some properties equal to action creators, here we're having an object equal to this {fetchPost: fetchPost} but cause property and value are written equal we can use ES6 shorthand and omit the property.
 )(PostList);
