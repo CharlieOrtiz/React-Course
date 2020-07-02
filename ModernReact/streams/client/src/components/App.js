af19de34133f0648ba15bc3a22d1ba7f63c627f8
Carlos3dg <carlosoo3dg@gmail.com>
2020-07-01 23:42:28 -0500

    Modern React: Uderstanding react-router - starting a streaming app

---
+++ ModernReact/streams/client/src/components/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import StreamCreate from './streams/streamCreate';
+import StreamDelete from './streams/streamDelete';
+import StreamEdit from './streams/streamEdit'
+import StreamList from './streams/streamList'
+import StreamShow from './streams/streamShow'
+import Header from './header';
+
+const App = () => {
+    return (
+        <div className='ui container'>
+            <BrowserRouter>
+                <div>
+                    <Header/>
+                    <Route path='/' exact component={StreamList}/>
+                    <Route path='/streams/new' component={StreamCreate}/>
+                    <Route path='/streams/edit' component={StreamEdit}/>
+                    <Route path='/streams/delete' component={StreamDelete}/>
+                    <Route path='/streams/show' component={StreamShow}/> 
+                </div>
+            </BrowserRouter>
+        </div>
+    );
+};
+
+export default App;
